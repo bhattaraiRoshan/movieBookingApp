2f94df7105de758e06b3ba36a72ea405650e27df
Roshan Bhattarai <roshan.bhattarai@proroshan.com>
2023-12-30 17:25:50 +1100

    Working on Select Elm

--- script.js
+++ script.js
@@ -1,19 +1,33 @@
 const seatElm = document.querySelectorAll('.seat')
+const selectElm = document.querySelector('#movie');
+const totalSeat = document.querySelector('#total');
+const movieName = document.querySelector('#moviename');
+const totalPrice = document.querySelector('#totalP');
+const containerSeat = document.querySelector('.container')
+const ticketPrice = +selectElm.value;
 
 
 
-seatElm.forEach((elm, index)=>{
+const updateSelectedSeat = () =>{
 
-    elm.addEventListener('click', ()=>{
-        
-        
+    const selectedSeat = document.querySelectorAll('.seat.selected');
+    let totalSeats = totalSeat.innerHTML = selectedSeat.length
+    totalPrice.innerHTML = totalSeats * ticketPrice
+  
+    
 
-        
-        elm.classList.value === 'seat selected' ? elm.classList.remove('selected') : elm.classList.add('selected')
-         if(elm.classList.value === 'seat occupied selected'){
-            alert('Seat has been already occupied. Please select another seat!') 
-            return
-         }
-    })
+}
+
+
+containerSeat.addEventListener('click', (e)=>{
+
+
+   if( e.target.classList.contains('seat') && !e.target.classList.contains('occupied')){
+
+       e.target.classList.toggle('selected')
+       updateSelectedSeat()
+
+}
 
 })
+
