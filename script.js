503a90743a5b04d95477c763275fb708199b5dbe
Roshan Bhattarai <roshan.bhattarai@proroshan.com>
2023-12-30 11:59:01 +1100

    Working on Script.js

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
-console.log('Hello World');
+const seatElm = document.querySelectorAll('.seat')
+
+
+
+seatElm.forEach((elm, index)=>{
+
+    elm.addEventListener('click', ()=>{
+        
+        
+
+        
+        elm.classList.value === 'seat selected' ? elm.classList.remove('selected') : elm.classList.add('selected')
+         if(elm.classList.value === 'seat occupied selected'){
+            alert('Seat has been already occupied. Please select another seat!') 
+            return
+         }
+    })
+
+})
